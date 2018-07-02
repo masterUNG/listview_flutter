a3fce8df30b96e7f217e462a56780fd45a38366c
MasterUNG <phrombutr@gmail.com>
2018-07-02 09:06:08 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(new MyStateLessWidget());
+}
+
+class MyStateLessWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(title: 'ListView Title',
+      home: new MyStateFulWidget(),);
+  }
+
+}
+
+class MyStateFulWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new MyState();
+  }
+
+}
+
+// ignore: type_argument_not_matching_bounds
+class MyState extends State<MyStateFulWidget>{
+
+  final myRandomWord = new WordPair.random();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('This is appBar'),),
+      body: new Center(child: new Text(myRandomWord.asUpperCase),),
+    );
+  }
+}
